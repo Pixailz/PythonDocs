641256a161392b0b17d32b2ce3d2153ba8d5be65
Pixailz <Pixailz1@gmail.com>
2021-02-11 17:32:56 +0100

    :sparkles:  : add FormationVideo/34_pygame_intro.py

---
+++ FormationVideo/34_pygame_intro.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+"""
+
+"""
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+launched = True
+
+while launched:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            launched = False
